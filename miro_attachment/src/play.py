6bef697606c09c27691234cd8e61540677171bad
TUoS Student <student@TENB885849C5626.shefuniad.shef.ac.uk>
2023-05-11 16:15:27 +0100

    play

--- miro_attachment/src/play.py
+++ miro_attachment/src/play.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-"""
-This script makes MiRo look for a blue ball and kick it
 
-The code was tested for Python 2 and 3
-For Python 2 you might need to change the shebang line to
-#!/usr/bin/env python
-"""
-# Imports
-##########################
 import os
 from math import radians  # This is used to reset the head pose
 import numpy as np  # Numerical Analysis library
 import cv2  # Computer Vision library
-from enum import Enum
+import random
+import time
 
 import rospy  # ROS Python interface
 from sensor_msgs.msg import CompressedImage  # ROS CompressedImage message
@@ -27,10 +20,8 @@ try:  # For convenience, import this util separately
     from miro2.lib import wheel_speed2cmd_vel  # Python 3
 except ImportError:
     from miro2.utils import wheel_speed2cmd_vel  # Python 2
-##########################
 
-
-class MiRoClient:
+class Play():
     """
     Script settings below
     """
@@ -62,6 +53,7 @@ class MiRoClient:
         """
         Wrapper to simplify driving MiRo by converting wheel speeds to cmd_vel
         """
+
         # Prepare an empty velocity command message
         msg_cmd_vel = TwistStamped()
 
@@ -222,10 +214,24 @@ class MiRoClient:
             self.ball[index] = self.detect_ball(image, index)
         # If no ball has been detected
         if not self.ball[0] and not self.ball[1]:
-            self.drive(self.SLOW, -self.SLOW)
+            randDir = random.randint(1, 3)
+            randTime = random.randint (1, 5)
+            if randDir == 1:
+                #self.drive(self.SLOW, -self.SLOW)
+                self.drive(self.FAST, self.SLOW)
+            elif randDir == 2:
+                self.drive(self.SLOW, self.FAST)
+            else: 
+                self.drive(self.SLOW, self.SLOW)
+            time.sleep(randTime)
         else:
             self.status_code = 2  # Switch to the second action
             self.just_switched = True
+
+    def search_surrounding(self):
+        randDir = random.randint(1, 3)
+        #atCliff = self.
+
 
     def lock_onto_ball(self, error=25):
         """
@@ -256,8 +262,10 @@ class MiRoClient:
             rotation_speed = 0.03  # Turn even slower now
             if abs(left_x) - abs(right_x) > error:
                 self.drive(rotation_speed, -rotation_speed)  # turn clockwise
+                #self.drive(self.FAST, self.SLOW)
             elif abs(left_x) - abs(right_x) < -error:
                 self.drive(-rotation_speed, rotation_speed)  # turn counterclockwise
+                #self.drive(self.SLOW, self.FAST)
             else:
                 # Successfully turned to face the ball
                 self.status_code = 3  # Switch to the third action
@@ -286,8 +294,8 @@ class MiRoClient:
 
     def __init__(self):
         # Initialise a new ROS node to communicate with MiRo
-        if not self.NODE_EXISTS:
-            rospy.init_node("kick_blue_ball", anonymous=True)
+        self.NODE_EXISTS = True
+        rospy.init_node("play", anonymous=True)
         # Give it some time to make sure everything is initialised
         rospy.sleep(2.0)
         # Initialise CV Bridge
@@ -332,216 +340,41 @@ class MiRoClient:
         # Move the head to default pose
         self.reset_head_pose()
 
-    def play(self):
-        # Step 1. Find ball
-        if self.status_code == 1:
-            # Every once in a while, look for ball
-            if self.counter % self.CAM_FREQ == 0:
-                self.look_for_ball()
-
-        # Step 2. Orient towards it
-        elif self.status_code == 2:
-            self.lock_onto_ball()
-
-        # Step 3. Kick!
-        elif self.status_code == 3:
-            self.kick()
-
-        # Fall back
-        else:
-            self.status_code = 1
-
-        # Yield
-        self.counter += 1
-
-    def detect_carer(self, frame, index):
-        if frame is None:  # Sanity check
-            return
-
-        # Debug window to show the frame
-        if self.DEBUG:
-            cv2.imshow("camera" + str(index), frame)
-            cv2.waitKey(1)
-
-        # Flag this frame as processed, so that it's not reused in case of lag
-        self.new_frame[index] = False
-        # Get image in HSV (hue, saturation, value) colour format
-        im_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-
-        # Specify target ball colour
-        rgb_colour = np.uint8([[[0, 0, 255]]])  # e.g. red (Note: BGR)
-        # Convert this colour to HSV colour model
-        hsv_colour = cv2.cvtColor(rgb_colour, cv2.COLOR_RGB2HSV)
-
-        # Extract colour boundaries for masking image
-        # Get the hue value from the numpy array containing target colour
-        target_hue = hsv_colour[0, 0][0]
-        hsv_lo_end = np.array([70, 70, target_hue - 20])
-        hsv_hi_end = np.array([255, 255, target_hue + 20])
-
-        # Generate the mask based on the desired hue range
-        mask = cv2.inRange(im_hsv, hsv_lo_end, hsv_hi_end)
-        mask_on_image = cv2.bitwise_and(im_hsv, im_hsv, mask=mask)
-
-        # Debug window to show the mask
-        if self.DEBUG:
-            cv2.imshow("mask" + str(index), mask_on_image)
-            cv2.waitKey(1)
-
-        # Clean up the image
-        seg = mask
-        seg = cv2.GaussianBlur(seg, (5, 5), 0)
-        seg = cv2.erode(seg, None, iterations=2)
-        seg = cv2.dilate(seg, None, iterations=2)
-
-        # Fine-tune parameters
-        ball_detect_min_dist_between_cens = 40  # Empirical
-        canny_high_thresh = 10  # Empirical
-        ball_detect_sensitivity = 10  # Lower detects more circles, so it's a trade-off
-        ball_detect_min_radius = 5  # Arbitrary, empirical
-        ball_detect_max_radius = 50  # Arbitrary, empirical
-
-        # Find circles using OpenCV routine
-        # This function returns a list of circles, with their x, y and r values
-        circles = cv2.HoughCircles(
-            seg,
-            cv2.HOUGH_GRADIENT,
-            1,
-            ball_detect_min_dist_between_cens,
-            param1=canny_high_thresh,
-            param2=ball_detect_sensitivity,
-            minRadius=ball_detect_min_radius,
-            maxRadius=ball_detect_max_radius,
-        )
-
-        if circles is None:
-            # If no circles were found, just display the original image
-            return
-
-        # Get the largest circle
-        max_circle = None
-        self.max_rad = 0
-        circles = np.uint16(np.around(circles))
-        for c in circles[0, :]:
-            if c[2] > self.max_rad:
-                self.max_rad = c[2]
-                max_circle = c
-        # This shouldn't happen, but you never know...
-        if max_circle is None:
-            return
-
-        # Append detected circle and its centre to the frame
-        cv2.circle(frame, (max_circle[0], max_circle[1]), max_circle[2], (0, 255, 0), 2)
-        cv2.circle(frame, (max_circle[0], max_circle[1]), 2, (0, 0, 255), 3)
-        if self.DEBUG:
-            cv2.imshow("circles" + str(index), frame)
-            cv2.waitKey(1)
-
-        # Normalise values to: x,y = [-0.5, 0.5], r = [0, 1]
-        max_circle = np.array(max_circle).astype("float32")
-        max_circle[0] -= self.x_centre
-        max_circle[0] /= self.frame_width
-        max_circle[1] -= self.y_centre
-        max_circle[1] /= self.frame_width
-        max_circle[1] *= -1.0
-        max_circle[2] /= self.frame_width
-
-        # Return a list values [x, y, r] for the largest circle
-        return [max_circle[0], max_circle[1], max_circle[2]]
-
-    def look_for_carer(self):
-        for index in range(2):  # For each camera (0 = left, 1 = right)
-            # Skip if there's no new image, in case the network is choking
-            if not self.new_frame[index]:
-                continue
-            image = self.input_camera[index]
-            # Run the detect ball procedure
-            self.ball[index] = self.detect_carer(image, index)
-        if not self.ball[0] and not self.ball[1]:
-            self.drive(self.FAST, self.SLOW)
-        else:
-            self.carer_found = True
-
-    def turn_to_carer(self):
-        self.reset_head_pose()
-        if not self.carer_found:
-            self.look_for_carer()
-        #turning:
-        else:
-            for index in range(2):  # For each camera (0 = left, 1 = right)
-                # Skip if there's no new image, in case the network is choking
-                if not self.new_frame[index]:
-                    continue
-                image = self.input_camera[index]
-                # Run the detect ball procedure
-                self.ball[index] = self.detect_carer(image, index)
-            if not self.ball[0] and self.ball[1]:
-                self.drive(self.SLOW, -self.SLOW)
-            # Conversely, rotate counterclockwise
-            elif self.ball[0] and not self.ball[1]:
-                self.drive(-self.SLOW, self.SLOW)
-            elif self.ball[0] and self.ball[1]:
-                error = 0.05  # 5% of image width
-                # Use the normalised values
-                left_x = self.ball[0][0]  # should be in range [0.0, 0.5]
-                right_x = self.ball[1][0]  # should be in range [-0.5, 0.0]
-                rotation_speed = 0.03  # Turn even slower now
-                if abs(left_x) - abs(right_x) > error:
-                    self.drive(rotation_speed, -rotation_speed)  # turn clockwise
-                elif abs(left_x) - abs(right_x) < -error:
-                    self.drive(-rotation_speed, rotation_speed)  # turn counterclockwise
-                else:
-                    # Successfully turned to face the ball
-                    self.bookmark = self.counter
-
-    def approach(self):
-        #print ("approach")
-        self.turn_to_carer()
-        self.counter += 1
-
     def loop(self):
         """
         Main control loop
         """
-        print("MiRo plays ball, press CTRL+C to halt...")
+        print("Miro plays")
         # Main control loop iteration counter
         self.counter = 0
         # This switch loops through MiRo behaviours:
         # Find ball, lock on to the ball and kick ball
         self.status_code = 0
-        self.carer_found = False
-        self.modes = Enum('mode', ['PLAY', 'APPROACH'])
-        self.mode = self.modes.APPROACH
-        self.need_for_care = 1
         while not rospy.core.is_shutdown():
 
-            if self.mode == self.modes.PLAY:
-                if self.counter == 1:
-                    print ("Miro in Play mode")
-                self.play()
-                if self.counter > 100: #need for care increases past threshold
-                    self.need_for_care += 1
-            elif self.mode == self.modes.APPROACH:
-                if self.counter == 1:
-                    print ("Miro in Approach mode")
-                    self.carer_found = False
-                self.approach()
-                if self.counter > 10: #satisfied need for care
-                    self.need_for_care -= 1
+            # Step 1. Find ball
+            if self.status_code == 1:
+                # Every once in a while, look for ball
+                if self.counter % self.CAM_FREQ == 0:
+                    self.look_for_ball()
+
+            # Step 2. Orient towards it
+            elif self.status_code == 2:
+                self.lock_onto_ball()
+
+            # Step 3. Kick!
+            elif self.status_code == 3:
+                self.kick()
+
+            # Fall back
+            else:
+                self.status_code = 1
+
+            # Yield
+            self.counter += 1
             rospy.sleep(self.TICK)
 
-            if self.need_for_care == 0 and self.mode != self.modes.PLAY:
-                print ("switch")
-                self.mode = self.modes.PLAY
-                self.counter = 0
-                self.status_code = 0
-            elif self.need_for_care == 1 and self.mode != self.modes.APPROACH:
-                print ("switch")
-                self.mode = self.modes.APPROACH
-                self.counter = 0
-            print (self.counter)
 
-# This condition fires when the script is called directly
 if __name__ == "__main__":
-    main = MiRoClient()  # Instantiate class
-    main.loop()  # Run the main control loop
+    play = Play()  # Instantiate class
+    play.loop()  # Run the main control loop
